b9ed19076c9f678c1134ed6b13075c26c795367c
Your Name <vemasani.pavani@gmail.com>
2026-02-02 13:58:09 +0530

    changed port

--- network.tf
+++ network.tf
@@ -2,8 +2,8 @@ resource "aws_security_group" "sg" {
   name = "${var.env}-sg"
   description = "${var.env}-sg"
   ingress {
-    from_port = 0
-    to_port = 0
+    from_port = 22
+    to_port = 22
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
