ab7b54f2a2c1e231a64e2a94146b6ef271c85a5f
Your Name <vemasani.pavani@gmail.com>
2026-02-02 13:56:27 +0530

    create a custom security group

---
+++ network.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "sg" {
+  name = "${var.env}-sg"
+  description = "${var.env}-sg"
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name="${var.env}-sg"
+  }
+}
