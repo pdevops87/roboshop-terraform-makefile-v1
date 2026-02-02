275af0354c8f350791c4cfc9893ae4ba7feecdc4
Your Name <vemasani.pavani@gmail.com>
2026-02-02 15:08:16 +0530

    attach security group to ec2

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance" {
   for_each = var.components
   ami           = var.ami
   instance_type = each.value["instance_type"]
+  security_groups = [aws_security_group.sg[each.key].id]
   tags = {
     Name = each.key
   }
