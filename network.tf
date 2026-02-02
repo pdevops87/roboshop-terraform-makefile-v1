b76e4889da80895c5f7ce056bfac9b434773ac5b
Your Name <vemasani.pavani@gmail.com>
2026-02-02 14:35:31 +0530

    loop the ports

--- network.tf
+++ network.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "sg" {
   dynamic "ingress" {
     for_each = var.appl_ports
     content {
-      from_port = ingress.value
-      to_port   = ingress.value
+      from_port = ingress.value.from_port
+      to_port   = ingress.value.to_port
       cidr_blocks = ["0.0.0.0/0"]
       protocol  = "tcp"
       description = ingress.key
